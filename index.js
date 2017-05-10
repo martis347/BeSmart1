7a8e079df3d7d6ff41369bbbcabf46617875aecb
Martynas Kanapinskas <martis347@gmail.com>
2017-05-10 14:15:43 +0300

    A

--- index.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var port = process.env.PORT || 1337;
-
-connect().use(serveStatic('./dist')).listen(port, function(){
-    console.log('Server running on ' + port + '...');
-});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,48 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ApiService } from './shared';
+import { routing } from './app.routing';
 
-import {TestComponent} from "./test/test.component";
+import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      { path: 'test', component: TestComponent },
-      { path: '', redirectTo: 'test', pathMatch: 'full' },
-      { path: '**', redirectTo: 'test', pathMatch: 'full' }
-    ]),
+    FormsModule,
+    routing
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  providers: [
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(public appRef: ApplicationRef) {}
+  hmrOnInit(store) {
+    console.log('HMR store', store);
+  }
+  hmrOnDestroy(store) {
+    let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // remove styles
+    removeNgStyles();
+  }
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
 }
